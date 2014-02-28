7dfbe40d5668026edd3835c57da0e6e4aab86bbe
Izaak Schroeder <izaakschroeder@aelatis.com>
2014-02-28 02:58:31 -0800

    Better evaluation information.

--- assets/scripts/assignment.js
+++ assets/scripts/assignment.js
@@ -9,5 +9,39 @@ require(['jquery', 'fuzzy-time'], function($) {
 		});
 
 		$('time').timeago();
+
+
+		var colors = {
+			
+		}
+
+		var letterGrades = {
+			0.95: 'A+',
+			0.9: 'A',
+			0.85: 'A-',
+			0.8: 'B+',
+			0.75: 'B',
+			0.7: 'B-',
+			0.65: 'C+',
+			0.6: 'C',
+			0.55: 'C-',
+			0.5: 'D',
+			0: 'F'
+		};
+
+		var entries = Object.keys(letterGrades).map(parseFloat).sort(function(a,b) {
+			return b - a;
+		})
+
+		$('.result[data-score]').each(function() {
+			var score = parseFloat($(this).data('score'));
+
+			var i = 0;
+			while (entries[i] > score)
+				++i;
+			
+
+			$(this).html((score*100).toFixed(2)+'%' + ' - ' + letterGrades[entries[i]]);
+		})
 	})
 });
