7ef15eae0ebd4e90bc5aca622424027eac91cc89
Izaak Schroeder <izaakschroeder@aelatis.com>
2014-02-28 01:05:34 -0800

    Updated startup.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-// FIXME: Is there a more elegant way?
-// If we're being called as node server.js or if we're being run under pm2 
-// then create the server and listen on the appropriate addresses/ports.
+
 var app = require('./lib/app');
-require('http').createServer(app).listen(8798); //, 'localhost'
+require('http').createServer(app).listen(process.env['PORT'] || 8798); //, 'localhost'
 
--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 # Mark as production environment
 export NODE_ENV="production" 
 
-# Start the HTTP front-end
-pm2 start -i max server.js
+pm2 reload www worker-run worker-evaluation
+
+# Start the HTTP front-end on 80 for now
+PORT=80 pm2 start -n www -i max server.js
 
 # Start the job workers
-pm2 start -x -i 2 ./lib/workers/run.js
-pm2 start -x -i 2 ./lib/workers/evaluation.js
+pm2 start -x -n worker-run -i 2 ./lib/workers/run.js
+pm2 start -x -n worker-evaluation -i 2 ./lib/workers/evaluation.js
