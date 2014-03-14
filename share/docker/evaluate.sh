37e2676c5e4e582fc86a167c85483baa969dbea5
Izaak Schroeder <izaakschroeder@aelatis.com>
2014-03-14 15:01:00 -0700

    Praise the lawd.

--- share/docker/evaluate.sh
+++ share/docker/evaluate.sh
@@ -1,18 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
-die() {
-	echo "$2" 1>&2;
-	exit $1
-}
 
 # Setup the build environment
- [ -f ./build ] || die 1 "Your project doesn't appear to have a build program."
+[ -f ./build ] || die 1 "Your project doesn't appear to have a build program."
 chmod +x ./build || die 1 "Unable to make build executable"
-./build || die 2 "Running build failed."
+./build
 
- [ -f ./run ] || die 1 "Your project doesn't appear to have a run program."
+[ -f ./run ] || die 1 "Your project doesn't appear to have a run program."
 chmod +x ./run || die 1 "Unable to make run executable"
-./run || die 2 "Running run failed."
+./run 
 
-# We're good for now lol
-exit 0
