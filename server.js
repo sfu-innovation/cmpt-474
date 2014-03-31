22208dc40e7602abc8fd5ec73dc64228a2d9cdcf
Izaak Schroeder <izaakschroeder@aelatis.com>
2014-03-31 13:31:18 -0700

    Working on support for peering into student submissions.

--- server.js
+++ server.js
@@ -2,5 +2,7 @@
 
 
 var app = require('./lib/app');
-require('http').createServer(app).listen(process.env['PORT'] || 8798); //, 'localhost'
-
+var app = require('./lib/app');
+require('http').createServer(app).on('connection', function(socket) {
+	socket.setTimeout(0); //Crafty
+}).listen(process.env['PORT'] || 8798); //, 'localhost'
